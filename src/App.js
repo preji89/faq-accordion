dab90e2f5e9893fd78c2a2c3f99a81b1bef94560
Prejita Mathalairajan <prejitamathalairajan@Prejitas-MacBook-Pro.local>
2023-02-02 12:24:19 +0530

    Add.FAQ Accordion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './styles.css';
+import Question from "./Question";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <h1>FAQ / Accordion</h1>
+    <div className="container">
+      <h2>Frequently Asked Questions</h2>
+      {
+        questions.map((question)=>(
+          <Question key={question.id} question={question}/>
+
+        ))
+      }
+    </div>
     </div>
   );
 }
+
+const questions = [
+  {
+    id: 1,
+    title: "Is this a good product?",
+    info:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem."
+  },
+  {
+    id: 2,
+    title: "How much does it cost?",
+    info:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem."
+  },
+  {
+    id: 3,
+    title: "When can I get it?",
+    info:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem."
+  },
+  {
+    id: 4,
+    title: "Do you ship world wide?",
+    info:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem."
+  }
+];
 
 export default App;
